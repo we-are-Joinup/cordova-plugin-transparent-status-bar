53f6b9ce4646719d63ab007559dc5ee98fe8676f
csi00153 <33608825+csi00153@users.noreply.github.com>
2018-02-07 22:15:21 +0100

    Update Transparentstatusbar.java

--- src/android/Transparentstatusbar.java
+++ src/android/Transparentstatusbar.java
@@ -8,6 +8,8 @@ import android.os.Build;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+//status bar height reference
+import android.content.res.Resources;
 
 public class Transparentstatusbar extends CordovaPlugin {
 
@@ -15,10 +17,21 @@ public class Transparentstatusbar extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("isTransparent")) {
             boolean result = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-            callbackContext.success(result ? 1 : 0);
+            int statusBarHeight = getStatusBarHeight();
+            callbackContext.success(result ? statusBarHeight : 0);
             return true;
         } else {
             return false;
         }
     }
+    
+    // A method to find height of the status bar
+        public int getStatusBarHeight() {
+                int result = 0;
+                int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+                if (resourceId > 0) {
+                        result = getResources().getDimensionPixelSize(resourceId);
+                }
+                return result;
+        }
 }
